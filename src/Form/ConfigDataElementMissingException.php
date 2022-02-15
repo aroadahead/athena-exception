3ab20e327e2850bbff78dbd61b901dd63caf9fd8
yonatonreid <yonatonreid@gmail.com>
2022-02-15 03:34:20 -0500

    upgrade to athena packages

---
+++ src/Form/ConfigDataElementMissingException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AthenaException\Form;
+
+use AthenaBridge\http\Exception\InvalidArgumentException;
+
+class ConfigDataElementMissingException extends InvalidArgumentException
+{
+
+}
